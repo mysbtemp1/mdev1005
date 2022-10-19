be7290f28a5f3bd5b928f1cb4794b9c71db8dcde
mysbtemp1 <88631365+mysbtemp1@users.noreply.github.com>
2022-10-19 20:18:00 +0530

    react setup

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* 👇️ Wrap your Route components in a Routes component */}
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
