cdf377563dc7ed5e576369f5758ec43fc237abd8
mysbtemp1 <88631365+mysbtemp1@users.noreply.github.com>
2022-10-20 15:47:53 +0530

    calculator

--- src/components/Tools/Calculator.js
+++ src/components/Tools/Calculator.js
@@ -1,7 +1,30 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+function calc(e)
+{
+    let display = document.getElementById("inputs");
+    
+    switch (e.target.value) 
+    {
+        case "C":
+            display.innerText = "";
+            break;
+        case "=":
+            try {
+                display.innerText = eval(display.innerText);
+            } catch {
+                display.innerText = "Error";
+            }
+            break;
+    
+        default:
+            display.innerText += e.target.innerText;
+    }
+}
+
 const Calculator = () => {
+    
     return (
         <div className='container'>
             <h1>This is Tools Page</h1>
@@ -13,7 +36,45 @@ const Calculator = () => {
                 </ul>
             </nav>
             <h1>This is Calculator</h1>
+            <div className="input">
+                <div type="text" id="inputs"></div>
+            </div>
+            <div className="buttons">
+                <div className="ops">
+                    <button className="button" value='+' onClick={(event) => calc(event)}>+</button>
+                    <button className="button" value='-' onClick={(event) => calc(event)}>-</button>
+                    <button className="button" value='*' onClick={(event) => calc(event)}>*</button>
+                    <button className="button" value='/' onClick={(event) => calc(event)}>/</button>
+                </div>
+                <div className="keypadj">
+                    <div className="keypadi">
+                        <button className="button" value='7' onClick={(event) => calc(event)}>7</button>
+                        <button className="button" value='4' onClick={(event) => calc(event)}>4</button>
+                        <button className="button" value='1' onClick={(event) => calc(event)}>1</button>
+                        <button className="button" value='0' onClick={(event) => calc(event)}>0</button>
+                    </div>
 
+
+                    <div className="keypadi">
+                        <button className="button" value='8' onClick={(event) => calc(event)}>8</button>
+                        <button className="button" value='5' onClick={(event) => calc(event)}>5</button>
+                        <button className="button" value='2' onClick={(event) => calc(event)}>2</button>
+                        <button className="button" value='.' onClick={(event) => calc(event)}>.</button>
+                    </div>
+
+                    <div className="keypadi">
+                        <button className="button" value='9' onClick={(event) => calc(event)}>9</button>
+                        <button className="button" value='6' onClick={(event) => calc(event)}>6</button>
+                        <button className="button" value='3' onClick={(event) => calc(event)}>3</button>
+                        <button className="button" value='C' onClick={(event) => calc(event)}>C</button>
+                    </div>
+
+
+                    <div className="keypadi">
+                        <button id="equal" className="button" value='=' onClick={(event) => calc(event)}>=</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
